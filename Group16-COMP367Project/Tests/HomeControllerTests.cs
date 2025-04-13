0ce34583d7e2e68cdd89f8e65917f0376e1323a9
mrakib4 <mrakib4@my.centennialcollege.ca>
2025-04-13 19:01:57 -0400

    Add test code for unit test

---
+++ Group16-COMP367Project/Tests/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Group16_COMP367Project.Controllers;
+
+namespace Group16_COMP367Project.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_ViewResult()
+        {
+            var controller = new HomeController(null);
+            var result = controller.Index();
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName); // Using default view
+        }
+
+        [Fact]
+        public void Privacy_Returns_ViewResult()
+        {
+            var controller = new HomeController(null);
+            var result = controller.Privacy();
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
